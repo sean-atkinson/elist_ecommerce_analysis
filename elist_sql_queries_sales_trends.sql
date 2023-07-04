b46ccc1429f65e5715015ba4136a93227a677336
Sean Atkinson <99100196+sean-atkinson@users.noreply.github.com>
2023-07-04 14:31:10 -0400

    Update elist_sql_queries_sales_trends.sql

--- elist_sql_queries_sales_trends.sql
+++ elist_sql_queries_sales_trends.sql
@@ -68,3 +68,33 @@ where extract(year from order_status.refund_ts) = 2021
     and lower(orders.product_name) like '%apple%'
 group by 1
 order by 1;
+
+--cleaning up product names
+--calculating refund rates for each product
+with refunds_cte as (
+    select case when product_name ='27in"" 4k gaming monitor' then '27in 4K gaming monitor' else product_name end as product_name_clean,
+        sum(case when order_status.refund_ts is not null then 1 else 0 end) as refunds,
+        round(sum(case when order_status.refund_ts is not null then 1 else 0 end)/count(distinct orders.id),3) as refund_rate,
+    from `elist-390902.elist.orders` orders
+    left join `elist-390902.elist.order_status` order_status
+        on orders.id = order_status.order_id
+    group by 1)
+--highlighing the 3 products with the highest refund rate
+select product_name_clean, refund_rate
+from refunds_cte
+order by 2 desc
+limit 3;
+
+with refunds_cte as (
+    select case when product_name ='27in"" 4k gaming monitor' then '27in 4K gaming monitor' else product_name end as product_name_clean,
+        sum(case when order_status.refund_ts is not null then 1 else 0 end) as refunds,
+        round(sum(case when order_status.refund_ts is not null then 1 else 0 end)/count(distinct orders.id),3) as refund_rate,
+    from `elist-390902.elist.orders` orders
+    left join `elist-390902.elist.order_status` order_status
+        on orders.id = order_status.order_id
+    group by 1)
+--highlighting the 3 products with the highest count of total refunds
+select product_name_clean, refunds
+from refunds_cte
+order by 2 desc
+limit 3;
