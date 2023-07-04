f0d011abfbbf3263a66840c009a8ba16a1dc9be8
Sean Atkinson <99100196+sean-atkinson@users.noreply.github.com>
2023-07-04 13:24:21 -0400

    Update elist_sql_queries_sales_trends.sql

--- elist_sql_queries_sales_trends.sql
+++ elist_sql_queries_sales_trends.sql
@@ -43,3 +43,29 @@ select round(avg(total_orders)) as avg_total_orders,
   round(avg(total_sales)) as avg_quarterly_sales,
   round(avg(avg_sales)) as avg_price
 from sales_trends_quarterly_cte;
+
+--counting the number of refunds per month (non-null values in refund_ts represent refunds)
+--calculting the refund rate
+with monthly_refunds_cte as (
+  select date_trunc(purchase_ts, month) as month,
+    sum(case when refund_ts is not null then 1 else 0 end) as refunds,
+    sum(case when refund_ts is not null then 1 else 0 end)/count(distinct order_id) as refund_rate
+from `elist-390902.elist.order_status` order_status
+group by 1
+order by 1)
+--calculating the monthly refund rate for 2020
+select round(avg(refund_rate),3) as monthly_refunds
+from monthly_refunds_cte
+where extract(year from month) = 2020;
+
+--calcultating the number of refunds per month (non-null values in refund_ts represent refunds)
+--filtering for 2021 and products with apple in their name
+select date_trunc(order_status.refund_ts, month) as month,
+    sum(case when order_status.refund_ts is not null then 1 else 0 end) as refunds,
+from `elist-390902.elist.order_status` order_status
+join `elist-390902.elist.orders` orders
+    on order_status.order_id = orders.id
+where extract(year from order_status.refund_ts) = 2021
+    and lower(orders.product_name) like '%apple%'
+group by 1
+order by 1;
